d1333a553869670a028405c3dfd3af8c4f5180f2
Alexey Malinin <amalininhere@gmail.com>
2018-10-26 21:16:08 +0700

    change test of `DefaultExternalTaskExecutor`

--- test/Camunda.Worker.Tests/Core/DefaultExternalTaskExecutorTest.cs
+++ test/Camunda.Worker.Tests/Core/DefaultExternalTaskExecutorTest.cs
@@ -49,7 +49,8 @@ namespace Camunda.Worker.Core
                 Variables = new Dictionary<string, Variable>()
             });
 
-            Assert.Equal(true, result["DONE"]?.Value);
+            Assert.True(result.TryGetValue("DONE", out var resultVariable));
+            Assert.True(Assert.IsType<bool>(resultVariable.Value));
         }
     }
 }
