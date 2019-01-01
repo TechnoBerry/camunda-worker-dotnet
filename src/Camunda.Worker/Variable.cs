84b57186315414e77c88778a5342a4334950425f
Alexey Malinin <amalininhere@gmail.com>
2019-01-01 17:32:57 +0700

    exclude constructors of `Variable` from coverage

--- src/Camunda.Worker/Variable.cs
+++ src/Camunda.Worker/Variable.cs
@@ -2,44 +2,53 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace Camunda.Worker
 {
     public class Variable
     {
+        [ExcludeFromCodeCoverage]
         public Variable(bool value)
         {
             Value = value;
             Type = VariableType.Boolean;
         }
 
+        [ExcludeFromCodeCoverage]
         public Variable(short value)
         {
             Value = value;
             Type = VariableType.Short;
         }
 
+        [ExcludeFromCodeCoverage]
         public Variable(int value)
         {
             Value = value;
             Type = VariableType.Integer;
         }
 
+        [ExcludeFromCodeCoverage]
         public Variable(long value)
         {
             Value = value;
             Type = VariableType.Long;
         }
 
+        [ExcludeFromCodeCoverage]
         public Variable(float value) : this((double) value)
         {
         }
 
+        [ExcludeFromCodeCoverage]
         public Variable(double value)
         {
             Value = value;
             Type = VariableType.Double;
         }
 
+        [ExcludeFromCodeCoverage]
         public Variable(string value)
         {
             Value = value;
