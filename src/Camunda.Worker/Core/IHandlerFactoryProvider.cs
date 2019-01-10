5e31d9aca4f72bb906868cb52dd1fc06c5048fb1
Alexey Malinin <amalininhere@gmail.com>
2019-01-10 20:23:59 +0700

    remove unused imports

--- src/Camunda.Worker/Core/IHandlerFactoryProvider.cs
+++ src/Camunda.Worker/Core/IHandlerFactoryProvider.cs
@@ -2,9 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
-using System;
-using Camunda.Worker.Execution;
-
 namespace Camunda.Worker.Core
 {
     public interface IHandlerFactoryProvider
