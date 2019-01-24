1d3e3264ed9d711d29b81e6c2984406fa3a5d1e5
Alexey Malinin <amalininhere@gmail.com>
2019-01-24 22:36:59 +0700

    add licence header to `IGeneralExternalTaskHandler.cs`

--- src/Camunda.Worker/Execution/IGeneralExternalTaskHandler.cs
+++ src/Camunda.Worker/Execution/IGeneralExternalTaskHandler.cs
@@ -1,3 +1,7 @@
+// Copyright (c) Alexey Malinin. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
 namespace Camunda.Worker.Execution
 {
     public interface IGeneralExternalTaskHandler : IExternalTaskHandler
