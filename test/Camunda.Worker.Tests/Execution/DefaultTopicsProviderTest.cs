cb0f63df1c13a0f9b42806223519c4246bfce463
Alexey Malinin <amalininhere@gmail.com>
2019-01-31 23:29:24 +0700

    add test for `DefaultTopicsProvider#GetTopics()`

---
+++ test/Camunda.Worker.Tests/Execution/DefaultTopicsProviderTest.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Camunda.Worker.Execution
+{
+    public class DefaultTopicsProviderTest
+    {
+        [Fact]
+        public void TestGetTopics()
+        {
+            IExternalTaskHandler Factory(IServiceProvider provider) => null;
+
+            var descriptors = new[]
+            {
+                new HandlerDescriptor("test1", Factory),
+                new HandlerDescriptor("test2", Factory)
+                {
+                    Variables = new[] {"X"},
+                    LockDuration = 10_000,
+                    LocalVariables = true
+                }
+            };
+
+            var topicsProvider = new DefaultTopicsProvider(descriptors);
+
+            var topics = topicsProvider.GetTopics().ToList();
+
+            Assert.Equal(2, topics.Count);
+
+            Assert.Equal("test1", topics[0].TopicName);
+            Assert.Null(topics[0].Variables);
+
+            Assert.Equal("test2", topics[1].TopicName);
+            Assert.NotNull(topics[1].Variables);
+            Assert.True(topics[1].LocalVariables);
+            Assert.Equal(10_000, topics[1].LockDuration);
+        }
+    }
+}
