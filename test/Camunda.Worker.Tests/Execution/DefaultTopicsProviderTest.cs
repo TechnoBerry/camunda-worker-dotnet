b6278c063cd4d8d0b4b843942414f153185c42d9
Alexey Malinin <amalininhere@gmail.com>
2019-01-31 23:42:14 +0700

    add licence header to `DefaultTopicProviderTest.cs`

--- test/Camunda.Worker.Tests/Execution/DefaultTopicsProviderTest.cs
+++ test/Camunda.Worker.Tests/Execution/DefaultTopicsProviderTest.cs
@@ -1,3 +1,7 @@
+// Copyright (c) Alexey Malinin. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
 using System;
 using System.Linq;
 using Xunit;
