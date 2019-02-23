7808b2ab404554299335af85e62bc0159fb2f833
Alexey Malinin <amalininhere@gmail.com>
2019-02-24 00:43:58 +0700

    check null arguments of constructor of `BpmnErrorResult`

--- src/Camunda.Worker/BpmnErrorResult.cs
+++ src/Camunda.Worker/BpmnErrorResult.cs
@@ -11,16 +11,16 @@ namespace Camunda.Worker
 {
     public sealed class BpmnErrorResult : IExecutionResult
     {
+        public BpmnErrorResult(string errorCode, string errorMessage, IDictionary<string, Variable> variables = null)
+        {
+            ErrorCode = Guard.NotNull(errorCode, nameof(errorCode));
+            ErrorMessage = Guard.NotNull(errorMessage, nameof(errorMessage));
+            Variables = variables ?? new Dictionary<string, Variable>();
+        }
+
         public string ErrorCode { get; }
         public string ErrorMessage { get; }
         public IDictionary<string, Variable> Variables { get; }
-
-        public BpmnErrorResult(string errorCode, string errorMessage, IDictionary<string, Variable> variables = null)
-        {
-            ErrorCode = errorCode;
-            ErrorMessage = errorMessage;
-            Variables = variables ?? new Dictionary<string, Variable>();
-        }
 
         public async Task ExecuteResult(ExternalTaskContext context)
         {
