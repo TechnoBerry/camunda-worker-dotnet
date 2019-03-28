ca1b628a11e389dcd4b627e5c17808bd02ef8453
Alexey Malinin <amalininhere@gmail.com>
2019-03-28 22:25:04 +0700

    use different instances of `ICamundaWorker` in hosted service

--- src/Camunda.Worker/Execution/WorkerHostedService.cs
+++ src/Camunda.Worker/Execution/WorkerHostedService.cs
@@ -1,12 +1,16 @@
 #region LICENSE
+
 // Copyright (c) Alexey Malinin. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
 #endregion
 
 
+using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 
@@ -14,19 +18,21 @@ namespace Camunda.Worker.Execution
 {
     public sealed class WorkerHostedService : BackgroundService
     {
-        private readonly ICamundaWorker _worker;
+        private readonly IServiceProvider _serviceProvider;
         private readonly CamundaWorkerOptions _options;
 
-        public WorkerHostedService(ICamundaWorker worker, IOptions<CamundaWorkerOptions> options)
+        public WorkerHostedService(IServiceProvider serviceProvider, IOptions<CamundaWorkerOptions> options)
         {
-            _worker = Guard.NotNull(worker, nameof(worker));
+            _serviceProvider = Guard.NotNull(serviceProvider, nameof(serviceProvider));
             _options = Guard.NotNull(options, nameof(options)).Value;
         }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
             var activeTasks = Enumerable.Range(0, _options.WorkerCount)
-                .Select(_ => _worker.Run(stoppingToken));
+                .Select(_ => _serviceProvider.GetRequiredService<ICamundaWorker>())
+                .Select(worker => worker.Run(stoppingToken))
+                .ToList();
             return Task.WhenAll(activeTasks);
         }
     }
--- test/Camunda.Worker.Tests/Execution/WorkerHostedServiceTest.cs
+++ test/Camunda.Worker.Tests/Execution/WorkerHostedServiceTest.cs
@@ -1,9 +1,12 @@
 #region LICENSE
+
 // Copyright (c) Alexey Malinin. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
 #endregion
 
 
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
@@ -15,12 +18,15 @@ namespace Camunda.Worker.Execution
     public class WorkerHostedServiceTest
     {
         private readonly Mock<ICamundaWorker> _workerMock = new Mock<ICamundaWorker>();
+        private readonly Mock<IServiceProvider> _providerMock = new Mock<IServiceProvider>();
 
         [Theory]
         [InlineData(1)]
         [InlineData(4)]
         public async Task TestRunWorkerOnStart(int workerCount)
         {
+            _providerMock.Setup(provider => provider.GetService(typeof(ICamundaWorker)))
+                .Returns(_workerMock.Object);
             _workerMock.Setup(w => w.Run(It.IsAny<CancellationToken>()))
                 .Returns(Task.CompletedTask);
 
@@ -30,11 +36,15 @@ namespace Camunda.Worker.Execution
             });
 
             using (var workerHostedService =
-                new WorkerHostedService(_workerMock.Object, options))
+                new WorkerHostedService(_providerMock.Object, options))
             {
                 await workerHostedService.StartAsync(CancellationToken.None);
             }
 
+            _providerMock.Verify(
+                provider => provider.GetService(typeof(ICamundaWorker)),
+                Times.Exactly(options.Value.WorkerCount)
+            );
             _workerMock.Verify(w => w.Run(It.IsAny<CancellationToken>()), Times.Exactly(options.Value.WorkerCount));
         }
     }
