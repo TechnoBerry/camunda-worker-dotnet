59fa8610a8c46e835a485a10951afdc4a34bc1dc
Alexey Malinin <amalininhere@gmail.com>
2019-04-09 21:54:11 +0700

    describe `IExecutionResult`

--- src/Camunda.Worker/IExecutionResult.cs
+++ src/Camunda.Worker/IExecutionResult.cs
@@ -8,8 +8,16 @@ using System.Threading.Tasks;
 
 namespace Camunda.Worker
 {
+    /// <summary>
+    /// Defines a contract that represents the result of external task's handler
+    /// </summary>
     public interface IExecutionResult
     {
+        /// <summary>
+        /// Executes the result operation of the external task's handler asynchronously
+        /// </summary>
+        /// <param name="context">The context in which the result is executed</param>
+        /// <returns>A task that represents the asynchronous execute operation.</returns>
         Task ExecuteResultAsync(IExternalTaskContext context);
     }
 }
