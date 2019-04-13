382d765468101e468fa798f7c0cdc630d6bfccf5
Alexey Malinin <amalininhere@gmail.com>
2019-04-13 14:34:00 +0700

    configure `serilog` in sample application

--- samples/SampleCamundaWorker/Program.cs
+++ samples/SampleCamundaWorker/Program.cs
@@ -1,16 +1,22 @@
 ﻿#region LICENSE
+
 // Copyright (c) Alexey Malinin. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
 #endregion
 
 
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Serilog;
 
 namespace SampleCamundaWorker
 {
     public class Program
     {
+        private const string LogTemplate = "{Timestamp:dd-MM-yyyy HH:mm:ss} [{Level:u4}] ({SourceContext}) " +
+                                           "{Message:lj}{NewLine}{Exception}";
+
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
@@ -18,6 +24,13 @@ namespace SampleCamundaWorker
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseSerilog((hostBuilder, logger) =>
+                {
+                    logger
+                        .ReadFrom.Configuration(hostBuilder.Configuration)
+                        .WriteTo.Console(outputTemplate: LogTemplate)
+                        .Enrich.FromLogContext();
+                });
     }
 }
