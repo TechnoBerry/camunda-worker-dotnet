cbe71f4bae950faa3af869d2094efac15c3b883f
Alexey Malinin <amalininhere@gmail.com>
2019-05-22 03:08:56 +0700

    remove importing of unused namespaces

--- src/Camunda.Worker/Execution/DefaultCamundaWorker.cs
+++ src/Camunda.Worker/Execution/DefaultCamundaWorker.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 
