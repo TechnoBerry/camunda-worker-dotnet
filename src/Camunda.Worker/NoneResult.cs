1e0155dd5f5306e4f106db7d348b0ef52606f78d
Alexey Malinin <amalininhere@gmail.com>
2019-11-10 15:23:56 +0700

    add `NoneResult` that doesn't complete external task

---
+++ src/Camunda.Worker/NoneResult.cs
@@ -0,0 +1,9 @@
+using System.Threading.Tasks;
+
+namespace Camunda.Worker
+{
+    public class NoneResult : IExecutionResult
+    {
+        public Task ExecuteResultAsync(IExternalTaskContext context) => Task.CompletedTask;
+    }
+}
---
+++ test/Camunda.Worker.Tests/NoneResultTest.cs
@@ -0,0 +1,19 @@
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+
+namespace Camunda.Worker
+{
+    public class NoneResultTest
+    {
+        private readonly Mock<IExternalTaskContext> _contextMock = new Mock<IExternalTaskContext>();
+
+        [Fact]
+        public async Task TestExecuteResultAsync()
+        {
+            var result = new NoneResult();
+            await result.ExecuteResultAsync(_contextMock.Object);
+            _contextMock.VerifyNoOtherCalls();
+        }
+    }
+}
