884eccac6ca4860b889c2f9479e8f9bd0d4a3bcc
Alexey Malinin <amalininhere@gmail.com>
2020-05-10 01:37:51 +0700

    simplify `ContextFactoryTest`

--- test/Camunda.Worker.Tests/Execution/ContextFactoryTest.cs
+++ test/Camunda.Worker.Tests/Execution/ContextFactoryTest.cs
@@ -1,33 +1,19 @@
 using System;
-using Camunda.Worker.Client;
 using Microsoft.Extensions.DependencyInjection;
-using Moq;
 using Xunit;
 
 namespace Camunda.Worker.Execution
 {
     public class ContextFactoryTest
     {
-        private readonly Mock<IServiceProvider> _providerMock = new Mock<IServiceProvider>();
-        private readonly Mock<IServiceScope> _scopeMock = new Mock<IServiceScope>();
         private readonly IContextFactory _factory;
 
         public ContextFactoryTest()
         {
-            _scopeMock.SetupGet(scope => scope.ServiceProvider).Returns(_providerMock.Object);
+            IServiceProvider provider = new ServiceCollection()
+                .BuildServiceProvider();
 
-            var scopeFactoryMock = new Mock<IServiceScopeFactory>();
-            scopeFactoryMock.Setup(factory => factory.CreateScope())
-                .Returns(_scopeMock.Object);
-
-            _providerMock.Setup(provider => provider.GetService(typeof(IServiceScopeFactory)))
-                .Returns(scopeFactoryMock.Object);
-
-            var clientMock = new Mock<IExternalTaskClient>();
-            _providerMock.Setup(provider => provider.GetService(typeof(IExternalTaskClient)))
-                .Returns(clientMock.Object);
-
-            _factory = new ContextFactory(_providerMock.Object);
+            _factory = new ContextFactory(provider);
         }
 
         [Fact]
@@ -38,7 +24,7 @@ namespace Camunda.Worker.Execution
             var result = _factory.MakeContext(task);
 
             Assert.Same(task, result.Task);
-            Assert.Same(_providerMock.Object, result.ServiceProvider);
+            Assert.NotNull(result.ServiceProvider);
         }
     }
 }
