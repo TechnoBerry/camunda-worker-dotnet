9f52b1c0062831a8abbeb5d148a7d12943d2872a
Alexey Malinin <amalininhere@gmail.com>
2021-09-20 19:50:27 +0700

    move logic from selector to worker

--- src/Camunda.Worker/Execution/DefaultCamundaWorker.cs
+++ src/Camunda.Worker/Execution/DefaultCamundaWorker.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading;
@@ -7,27 +8,34 @@ using Camunda.Worker.Client;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
 
 namespace Camunda.Worker.Execution
 {
     public sealed class DefaultCamundaWorker : ICamundaWorker
     {
-        private readonly IExternalTaskSelector _selector;
         private readonly IExternalTaskClient _externalTaskClient;
+        private readonly ITopicsProvider _topicsProvider;
+        private readonly CamundaWorkerOptions _workerOptions;
+        private readonly SelectorOptions _selectorOptions;
         private readonly IServiceScopeFactory _serviceScopeFactory;
         private readonly WorkerHandlerDescriptor _workerHandlerDescriptor;
         private readonly ILogger<DefaultCamundaWorker> _logger;
 
         public DefaultCamundaWorker(
-            IExternalTaskSelector selector,
             IExternalTaskClient externalTaskClient,
+            ITopicsProvider topicsProvider,
+            CamundaWorkerOptions workerOptions,
+            IOptions<SelectorOptions> selectorOptions,
             IServiceScopeFactory serviceScopeFactory,
             WorkerHandlerDescriptor workerHandlerDescriptor,
             ILogger<DefaultCamundaWorker>? logger = null
         )
         {
-            _selector = Guard.NotNull(selector, nameof(selector));
             _externalTaskClient = Guard.NotNull(externalTaskClient, nameof(externalTaskClient));
+            _topicsProvider = Guard.NotNull(topicsProvider, nameof(topicsProvider));
+            _workerOptions = Guard.NotNull(workerOptions, nameof(workerOptions));
+            _selectorOptions = Guard.NotNull(selectorOptions, nameof(selectorOptions)).Value;
             _serviceScopeFactory = Guard.NotNull(serviceScopeFactory, nameof(serviceScopeFactory));
             _workerHandlerDescriptor = Guard.NotNull(workerHandlerDescriptor, nameof(workerHandlerDescriptor));
             _logger = logger ?? NullLogger<DefaultCamundaWorker>.Instance;
@@ -37,7 +45,7 @@ namespace Camunda.Worker.Execution
         {
             while (!cancellationToken.IsCancellationRequested)
             {
-                var externalTasks = await _selector.SelectAsync(cancellationToken);
+                var externalTasks = await SelectAsync(cancellationToken);
 
                 var executableTasks = externalTasks
                     .Select(ProcessExternalTask)
@@ -46,6 +54,41 @@ namespace Camunda.Worker.Execution
                 await Task.WhenAll(executableTasks);
             }
         }
+
+        private async Task<IReadOnlyCollection<ExternalTask>> SelectAsync(CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                Log.Waiting(_logger);
+                var fetchAndLockRequest = MakeRequestBody();
+                var externalTasks = await _externalTaskClient.FetchAndLockAsync(fetchAndLockRequest, cancellationToken);
+                Log.Locked(_logger, externalTasks.Count);
+                return externalTasks;
+            }
+            catch (Exception e) when (!cancellationToken.IsCancellationRequested)
+            {
+                Log.FailedLocking(_logger, e);
+                await DelayOnFailure(cancellationToken);
+                return Array.Empty<ExternalTask>();
+            }
+        }
+
+        private FetchAndLockRequest MakeRequestBody()
+        {
+            var topics = _topicsProvider.GetTopics();
+
+            var fetchAndLockRequest = new FetchAndLockRequest(_workerOptions.WorkerId, _selectorOptions.MaxTasks)
+            {
+                UsePriority = _selectorOptions.UsePriority,
+                AsyncResponseTimeout = _selectorOptions.AsyncResponseTimeout,
+                Topics = topics
+            };
+
+            return fetchAndLockRequest;
+        }
+
+        private static Task DelayOnFailure(CancellationToken cancellationToken) =>
+            Task.Delay(10_000, cancellationToken);
 
         private async Task ProcessExternalTask(ExternalTask externalTask)
         {
@@ -65,12 +108,39 @@ namespace Camunda.Worker.Execution
         [ExcludeFromCodeCoverage]
         private static class Log
         {
+            private static readonly Action<ILogger, Exception?> _waiting =
+                LoggerMessage.Define(
+                    LogLevel.Debug,
+                    new EventId(0),
+                    "Waiting for external task"
+                );
+
+            private static readonly Action<ILogger, int, Exception?> _locked =
+                LoggerMessage.Define<int>(
+                    LogLevel.Debug,
+                    new EventId(0),
+                    "Locked {Count} external tasks"
+                );
+
+            private static readonly Action<ILogger, string, Exception?> _failedLocking =
+                LoggerMessage.Define<string>(
+                    LogLevel.Warning,
+                    new EventId(0),
+                    "Failed locking of external tasks. Reason: \"{Reason}\""
+                );
+
             private static readonly Action<ILogger, string, Exception?> _failedExecution =
                 LoggerMessage.Define<string>(
                     LogLevel.Warning,
                     new EventId(0),
                     "Failed execution of task {Id}"
                 );
+
+            public static void Waiting(ILogger logger) => _waiting(logger, null);
+
+            public static void Locked(ILogger logger, int count) => _locked(logger, count, null);
+
+            public static void FailedLocking(ILogger logger, Exception e) => _failedLocking(logger, e.Message, e);
 
             public static void FailedExecution(ILogger logger, string externalTaskId, Exception e) =>
                 _failedExecution(logger, externalTaskId, e);
--- test/Camunda.Worker.Tests/Execution/DefaultCamundaWorkerTest.cs
+++ test/Camunda.Worker.Tests/Execution/DefaultCamundaWorkerTest.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Bogus;
 using Camunda.Worker.Client;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Moq;
 using Xunit;
 
@@ -14,7 +16,12 @@ namespace Camunda.Worker.Execution
     {
         private readonly Mock<IHandler> _handlerMock = new();
         private readonly Mock<IExternalTaskClient> _clientMock = new();
-        private readonly Mock<IExternalTaskSelector> _selectorMock = new();
+        private readonly Mock<ITopicsProvider> _topicsProviderMock = new();
+        private readonly CamundaWorkerOptions _workerOptions = new("testWorker");
+        private readonly IOptions<SelectorOptions> _selectorOptions = Options.Create(new SelectorOptions
+        {
+            AsyncResponseTimeout = 5_000
+        });
         private readonly ServiceProvider _serviceProvider;
         private readonly DefaultCamundaWorker _worker;
 
@@ -22,8 +29,10 @@ namespace Camunda.Worker.Execution
         {
             _serviceProvider = new ServiceCollection().BuildServiceProvider();
             _worker = new DefaultCamundaWorker(
-                _selectorMock.Object,
                 _clientMock.Object,
+                _topicsProviderMock.Object,
+                _workerOptions,
+                _selectorOptions,
                 _serviceProvider.GetRequiredService<IServiceScopeFactory>(),
                 new WorkerHandlerDescriptor(_handlerMock.Object.HandleAsync)
             );
@@ -52,8 +61,8 @@ namespace Camunda.Worker.Execution
 
             var cts = new CancellationTokenSource();
 
-            _selectorMock
-                .Setup(selector => selector.SelectAsync(It.IsAny<CancellationToken>()))
+            _clientMock
+                .Setup(selector => selector.FetchAndLockAsync(It.IsAny<FetchAndLockRequest>(), It.IsAny<CancellationToken>()))
                 .Callback(cts.Cancel)
                 .ReturnsAsync(externalTasks)
                 .Verifiable();
@@ -66,11 +75,31 @@ namespace Camunda.Worker.Execution
             await _worker.RunAsync(cts.Token);
 
             // Assert
-            _selectorMock.VerifyAll();
             _handlerMock.Verify(
                 executor => executor.HandleAsync(It.IsAny<IExternalTaskContext>()),
                 Times.Exactly(numberOfExternalTasks)
             );
+        }
+
+        [Fact]
+        public async Task TestCancelledSelection()
+        {
+            var cts = new CancellationTokenSource();
+            var tcs = new TaskCompletionSource<List<ExternalTask>>();
+
+            await using var reg = cts.Token.Register(() => tcs.SetCanceled());
+
+            _clientMock
+                .Setup(client =>
+                    client.FetchAndLockAsync(It.IsAny<FetchAndLockRequest>(), It.IsAny<CancellationToken>()))
+                .Returns(tcs.Task);
+
+            var resultTask = _worker.RunAsync(cts.Token);
+
+            cts.Cancel();
+
+            await Assert.ThrowsAsync<TaskCanceledException>(() => resultTask);
+            _clientMock.VerifyAll();
         }
 
         public interface IHandler
