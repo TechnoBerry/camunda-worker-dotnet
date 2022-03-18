08017b3a9fc9e252139bef33189a2f17d5e4217f
Ionut Grosu <ifl@openminds.dk>
2022-03-18 13:25:58 +0100

    Added test case for the new functionality

--- test/Camunda.Worker.Tests/CamundaWorkerBuilderExtensionsTest.cs
+++ test/Camunda.Worker.Tests/CamundaWorkerBuilderExtensionsTest.cs
@@ -90,6 +90,45 @@ namespace Camunda.Worker
             Assert.Throws<Exception>(() => _builderMock.Object.AddHandler<HandlerWithoutTopics>());
         }
 
+        [Fact]
+        public void TestAddHandlerWithAllVariables()
+        {
+            // Arrange
+            var savedMetadata = new List<HandlerMetadata>();
+
+            _builderMock
+                .Setup(builder => builder.AddHandler(It.IsAny<ExternalTaskDelegate>(), It.IsAny<HandlerMetadata>()))
+                .Callback((ExternalTaskDelegate _, HandlerMetadata metadata) => savedMetadata.Add(metadata))
+                .Returns(_builderMock.Object);
+
+            // Act
+            _builderMock.Object.AddHandler<HandlerWithAllVariables>();
+
+            // Assert
+            _builderMock.Verify(
+                builder => builder.AddHandler(It.IsAny<ExternalTaskDelegate>(), It.IsAny<HandlerMetadata>()),
+                Times.Once());
+            Assert.Contains(_services, d => d.Lifetime == ServiceLifetime.Transient &&
+                                            d.ServiceType == typeof(HandlerWithAllVariables));
+
+            var metadata = Assert.Single(savedMetadata);
+            Assert.NotNull(metadata);
+            Assert.Null(metadata.Variables);
+        }
+
+        [HandlerTopics("testTopic")]
+        [HandlerVariables(AllVariables = true)]
+        private class HandlerWithAllVariables : IExternalTaskHandler
+        {
+            public Task<IExecutionResult> HandleAsync(ExternalTask externalTask, CancellationToken cancellationToken)
+            {
+                return Task.FromResult<IExecutionResult>(new CompleteResult
+                {
+                    Variables = externalTask.Variables
+                });
+            }
+        }
+
         [HandlerTopics("testTopic_1", "testTopic_1")]
         [HandlerVariables("testVariable", LocalVariables = true)]
         private class HandlerWithTopics : IExternalTaskHandler
