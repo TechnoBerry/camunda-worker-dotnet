0587cf25ac38c2dd0a616cd8c707e2f6941045c2
Alexey Malinin <amalininhere@gmail.com>
2022-10-18 02:31:21 +0700

    remove obsolete methods from `IExternalTaskContext`

--- src/Camunda.Worker/Execution/ExternalTaskContext.cs
+++ src/Camunda.Worker/Execution/ExternalTaskContext.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.Threading;
-using System.Threading.Tasks;
 using Camunda.Worker.Client;
 
 namespace Camunda.Worker.Execution;
@@ -28,53 +26,4 @@ public sealed class ExternalTaskContext : IExternalTaskContext
     public IServiceProvider ServiceProvider { get; }
 
     public CancellationToken ProcessingAborted { get; }
-
-    public async Task ExtendLockAsync(int newDuration)
-    {
-        var request = new ExtendLockRequest(Task.WorkerId, newDuration);
-        await Client.ExtendLockAsync(Task.Id, request);
-    }
-
-    public async Task CompleteAsync(
-        IDictionary<string, Variable>? variables = null,
-        IDictionary<string, Variable>? localVariables = null
-    )
-    {
-        var request = new CompleteRequest(Task.WorkerId)
-        {
-            Variables = variables,
-            LocalVariables = localVariables
-        };
-        await Client.CompleteAsync(Task.Id, request);
-    }
-
-    public async Task ReportFailureAsync(
-        string? errorMessage,
-        string? errorDetails,
-        int? retries = default,
-        int? retryTimeout = default
-    )
-    {
-        var request = new ReportFailureRequest(Task.WorkerId)
-        {
-            ErrorMessage = errorMessage,
-            ErrorDetails = errorDetails,
-            Retries = retries,
-            RetryTimeout = retryTimeout
-        };
-        await Client.ReportFailureAsync(Task.Id, request);
-    }
-
-    public async Task ReportBpmnErrorAsync(
-        string errorCode,
-        string errorMessage,
-        IDictionary<string, Variable>? variables = null
-    )
-    {
-        var request = new BpmnErrorRequest(Task.WorkerId, errorCode, errorMessage)
-        {
-            Variables = variables
-        };
-        await Client.ReportBpmnErrorAsync(Task.Id, request);
-    }
 }
