d5888772c537915c669da851d8a9568b491e67aa
Alexey Malinin <amalininhere@gmail.com>
2022-10-27 04:05:09 +0700

    add helpers to use json variables with typed objects

--- src/Camunda.Worker/Variables/JsonVariable.cs
+++ src/Camunda.Worker/Variables/JsonVariable.cs
@@ -1,5 +1,21 @@
+using System;
+using System.Text.Json;
 using System.Text.Json.Nodes;
 
 namespace Camunda.Worker.Variables;
 
-public record JsonVariable(JsonNode Value) : VariableBase;
+public record JsonVariable(JsonNode Value) : VariableBase
+{
+    public static JsonVariable Create<T>(T value, JsonSerializerOptions? options = null)
+    {
+        var jsonNode = JsonSerializer.SerializeToNode(value, options)
+                       ?? throw new ArgumentException("Given object could not be serialized to json", nameof(value));
+
+        return new JsonVariable(jsonNode);
+    }
+
+    public T? Parse<T>(JsonSerializerOptions? options = null)
+    {
+        return Value.Deserialize<T>(options);
+    }
+}
