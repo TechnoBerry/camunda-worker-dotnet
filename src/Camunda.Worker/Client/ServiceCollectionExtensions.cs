9fcf169a816ddd402a8dfce6d03077b6a4b3b803
anserg256 <anserg256@gmail.com>
2023-11-13 23:47:56 +0400

    Small refactoring of extensions

--- src/Camunda.Worker/Client/ServiceCollectionExtensions.cs
+++ src/Camunda.Worker/Client/ServiceCollectionExtensions.cs
@@ -9,19 +9,18 @@ public static class ServiceCollectionExtensions
 {
     public static IHttpClientBuilder AddExternalTaskClient(this IServiceCollection services)
     {
-        return services.AddHttpClient<IExternalTaskClient>()
-            .AddTypedClient<IExternalTaskClient>(httpClient => new ExternalTaskClient(httpClient));
+        return services.AddHttpClient<IExternalTaskClient, ExternalTaskClient>(
+            httpClient => new ExternalTaskClient(httpClient));
     }
 
     public static IHttpClientBuilder AddExternalTaskClient(this IServiceCollection services, Action<HttpClient> configureClient)
     {
-        return services.AddHttpClient<IExternalTaskClient>(configureClient)
-            .AddTypedClient<IExternalTaskClient>(httpClient => new ExternalTaskClient(httpClient));
+        return AddExternalTaskClient(services).ConfigureHttpClient(configureClient);
     }
 
     public static IHttpClientBuilder AddExternalTaskClient(this IServiceCollection services, Action<HttpClient> configureClient, Action<JsonSerializerOptions> configureJsonOptions)
     {
-        return services.AddHttpClient<IExternalTaskClient>(configureClient)
-            .AddTypedClient<IExternalTaskClient>(httpClient => new ExternalTaskClient(httpClient, configureJsonOptions));
+        return services.AddHttpClient<IExternalTaskClient, ExternalTaskClient>(
+            httpClient => new ExternalTaskClient(httpClient, configureJsonOptions)).ConfigureHttpClient(configureClient);
     }
 }
